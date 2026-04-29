a051e5a6f43b3d2035b0a90312e962dcfe817cd1
danixantosl-oss <danixantosl@gmail.com>
2026-04-28 23:32:06 -0500

    Add gz_sim.launch.py for Gazebo simulation launch

---
+++ launch/gz_sim.launch.py
@@ -0,0 +1,19 @@
+from launch import LaunchDescription
+from launch.actions import IncludeLaunchDescription
+from launch.launch_description_sources import PythonLaunchDescriptionSource
+from ament_index_python.packages import get_package_share_directory
+import os
+
+def generate_launch_description():
+
+    pkg_ros_gz_sim = get_package_share_directory('ros_gz_sim')
+
+    gz_sim = IncludeLaunchDescription(
+        PythonLaunchDescriptionSource(
+            os.path.join(pkg_ros_gz_sim, 'launch', 'gz_sim.launch.py')
+        )
+    )
+
+    return LaunchDescription([
+        gz_sim
+    ])
